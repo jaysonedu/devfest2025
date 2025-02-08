d11a7a656f8432a34a99b422e4725040cb93017d
Emujin <emujintsogtjargal@gmail.com>
2025-02-08 15:29:13 -0500

    created header component and landing page

---
+++ app/quiz/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "@/components/Header";
+
+function page() {
+  return (
+    <div className="min-h-screen bg-white">
+      <Header />
+    </div>
+  );
+}
+
+export default page;
